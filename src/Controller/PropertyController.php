b242b36627df99b5738579380d0e0435a0203db9
driiich2 <geekdugrenier2@gmail.com>
2019-03-22 15:17:40 +0100

    Fin Pagination

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Property;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,17 +35,20 @@ class PropertyController extends AbstractController
 
     /**
      * @Route("/biens", name="property.index")
-     * @param PropertyRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
      * @return Response
      */
-    public function index(PropertyRepository $repository): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
-        $property = $repository->findLatest();
-        $property[0]->setSold(true);
-        $this->em->flush();
-        dump($property);
+        $properties = $paginator->paginate(
+            $this->repository->findAllVisibleQuery(),
+            $request->query->getInt('page', 1),
+            12
+        );
         return $this->render('property/index.html.twig', [
-            'current_menu' => 'properties'
+            'current_menu' => 'properties',
+            'properties' => $properties
         ]);
     }
 
--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Property;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -20,16 +21,13 @@ class PropertyRepository extends ServiceEntityRepository
         parent::__construct($registry, Property::class);
     }
 
-
-
     /**
-     * @return Property[]
+     * @return Query
      */
-    public function findAllVisible() : array
+    public function findAllVisibleQuery() : Query
     {
         return $this->findVisibleQuery()
-            ->getQuery()
-            ->getResult();
+            ->getQuery();
     }
 
     /**
