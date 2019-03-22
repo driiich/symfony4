755d911be9ebf488a42ec34c2cf910c66c5b98f0
driiich2 <geekdugrenier2@gmail.com>
2019-03-22 16:57:26 +0100

    Fin Filtre

--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\QueryBuilder;
@@ -22,18 +23,30 @@ class PropertyRepository extends ServiceEntityRepository
     }
 
     /**
+     * @param PropertySearch $search
      * @return Query
      */
-    public function findAllVisibleQuery() : Query
+    public function findAllVisibleQuery(PropertySearch $search): Query
     {
-        return $this->findVisibleQuery()
-            ->getQuery();
+        $query = $this->findVisibleQuery();
+
+        if ($search->getMaxPrice()) {
+            $query = $query
+                ->andWhere('p.price <= :maxprice')
+                ->setParameter('maxprice', $search->getMaxPrice());
+        }
+        if ($search->getMinSurface()) {
+            $query = $query
+                ->andWhere('p.surface >= :minsurface')
+                ->setParameter('minsurface', $search->getMinSurface());
+        }
+        return $query->getQuery();
     }
 
     /**
      * @return Property[]
      */
-    public function findLatest() : array
+    public function findLatest(): array
     {
         return $this->findVisibleQuery()
             ->setMaxResults(4)
@@ -41,7 +54,7 @@ class PropertyRepository extends ServiceEntityRepository
             ->getResult();
     }
 
-    private function findVisibleQuery() : QueryBuilder
+    private function findVisibleQuery(): QueryBuilder
     {
         return $this->createQueryBuilder('p')
             ->Where('p.sold = false');
